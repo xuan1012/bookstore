996a01f337505f406eaa610bba71e2db9123ab73
xuan_mx <506243021@qq.com>
2019-06-04 19:59:21 +0800

    集成支付宝完工

---
+++ src/main/java/com/bookstore/bookstore/dao/OrderbuyMapper.java
@@ -0,0 +1,16 @@
+package com.bookstore.bookstore.dao;
+
+import com.bookstore.bookstore.dao.model.Orderbuy;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 订单表 Mapper 接口
+ * </p>
+ *
+ * @author xuan
+ * @since 2019-06-04
+ */
+public interface OrderbuyMapper extends BaseMapper<Orderbuy> {
+
+}
---
+++ src/main/java/com/bookstore/bookstore/dao/model/Ordergroup.java
@@ -0,0 +1,38 @@
+package com.bookstore.bookstore.dao.model;
+
+    import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+* <p>
+    * 
+    * </p>
+*
+* @author xuan
+* @since 2019-06-04
+*/
+    @Data
+        @EqualsAndHashCode(callSuper = false)
+    @Accessors(chain = true)
+    public class Ordergroup implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+            @TableId(value = "group_id", type = IdType.AUTO)
+    private Long groupId;
+
+    private String groupContent;
+
+    private BigDecimal money;
+
+    private Date creatDate;
+
+
+}
