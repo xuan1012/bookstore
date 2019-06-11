78771e727e1d83724e75f61fc40709bc72611669
xuan_mx <506243021@qq.com>
2019-06-11 12:40:47 +0800

    整合

--- src/main/java/com/bookstore/bookstore/web/CommentController.java
+++ src/main/java/com/bookstore/bookstore/web/CommentController.java
@@ -1,9 +1,16 @@
 package com.bookstore.bookstore.web;
 
 
+import com.bookstore.bookstore.dao.model.Comment;
+import com.bookstore.bookstore.service.ICommentService;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.util.DateUtils;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/comment")
 public class CommentController {
+    @Resource
+    ICommentService commentService;
 
+    @RequestMapping("add")
+    public Map<String, Object> addComment(Comment comment) {
+        Date now = DateUtils.createNow().getTime();
+        comment.setCommentDate(now);
+        commentService.save(comment);
+        return new HashMap<>(1);
+    }
 }
